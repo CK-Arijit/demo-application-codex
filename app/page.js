66ba68c9b5cad12db6de2cf0aea8ed0c8d344634
CK-Arijit <arijit.naskar@cloudkaptan.com>
2026-03-23 16:31:51 +0530

    changes made

--- app/page.js
+++ app/page.js
@@ -2,7 +2,9 @@ export default function HomePage() {
   return (
     <main className="container">
       <h1>Next.js App is Ready</h1>
-      <p>Edit <code>app/page.js</code> to start building.</p>
+      <p>
+        Edit <code>app/page.js</code> to start building.
+      </p>
     </main>
   );
 }
